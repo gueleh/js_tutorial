85210a4cf6309aa0dd65e8ccc21510016c701009
Günther Lehner <guenther.lehner@protonmail.com>
2018-06-09 12:16:15 +0200

    Add reverse() to String prototype.

--- palindrome.js
+++ palindrome.js
@@ -1,18 +1,35 @@
 // Reverses a string.
-function reverse(string) {
-	return Array.from(string).reverse().join("");
+String.prototype.reverse = function() {
+	return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
 	this.content = content;
 
-	this.palindrome = function palindrome() {
-		let processedContent = this.content.toLowerCase();
-		return processedContent === reverse(processedContent);
+	this.processor = function(string) {
+		return string.toLowerCase();
 	}
 
-	this.louder = function louder() {
-		return this.content.toUpperCase();
+	// Returns content processed for palindrome testing
+	this.processedContent = function() {
+		return this.processor(this.content);
+	}
+
+	this.palindrome = function() {
+		return this.processedContent() === this.processedContent().reverse();
+	}
+
+}
+
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	// Returns translation processed for palindrome testing
+	this.processedContent = function processedContent() {
+		return this.processor(this.translation);
 	}
 }
+
+TranslatedPhrase.prototype = new Phrase();
