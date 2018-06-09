2441f50ae04e40a948dc770019a9887fa0c6ce46
Günther Lehner <guenther.lehner@protonmail.com>
2018-06-09 11:38:44 +0200

    Learn about objects

--- palindrome.js
+++ palindrome.js
@@ -3,9 +3,16 @@ function reverse(string) {
 	return Array.from(string).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-	let processedContent = string.toLowerCase();
-	return processedContent === reverse(processedContent);
-}
+// Defines a Phrase object.
+function Phrase(content) {
+	this.content = content;
 
+	this.palindrome = function palindrome() {
+		let processedContent = this.content.toLowerCase();
+		return processedContent === reverse(processedContent);
+	}
+
+	this.louder = function louder() {
+		return this.content.toUpperCase();
+	}
+}
